696e18019a76e9eab095a29c4632b3db48692216
경석 <yks9364@naver.com>
2017-03-22 21:16:49 +0900

    commit!!!!!

--- PeaceNara/src/hk02/main/Main.java
+++ PeaceNara/src/hk02/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("first commit");
+		System.out.println("first commit !!");
 	}
 
 }
