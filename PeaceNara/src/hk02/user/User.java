69718730776edf627b2312a347b29c3c67cb671c
piopoi <tarophin00@gmail.com>
2017-03-23 10:38:07 +0900

    1st commit. hk02.user 패키지와 User클래스 추가

---
+++ PeaceNara/src/hk02/user/User.java
@@ -0,0 +1,13 @@
+package hk02.user;
+
+import java.util.ArrayList;
+
+public class User {
+
+	private String id;
+	private String pw;
+	private int balance;
+//	private ArrayList<Goods> uploadedGoods; 
+	
+	
+}
