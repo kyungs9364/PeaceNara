affa118e2fb21b5069b6ebcf4336a4a2f3e9b741
piopoi <tarophin00@gmail.com>
2017-03-23 18:59:59 +0900

    Update User, MyPage -강석원

--- PeaceNara/src/hk02/user/User.java
+++ PeaceNara/src/hk02/user/User.java
@@ -4,10 +4,40 @@ import java.util.ArrayList;
 
 public class User {
 
-	private String id;
-	private String pw;
-	private int balance;
-//	private ArrayList<Goods> uploadedGoods; 
+	private String id; // ID
+	private String pw; // Password
+	private int balance; // 잔액
+	private ArrayList<Goods> uploadedGoodsList; // 팔려고 업로드한 상품 리스트
 	
+	public User(String id, String pw, int balance){
+		this.id = id;
+		this.pw = pw;
+		this.balance = balance;
+		this.uploadedGoodsList = null; // goods클래스 올라오면 수정
+	}
 	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	public ArrayList<Goods> getUploadedGoodsList() {
+		return uploadedGoodsList;
+	}
+	public void setUploadedGoodsList(ArrayList<Goods> uploadedGoodsList) {
+		this.uploadedGoodsList = uploadedGoodsList;
+	}
 }
