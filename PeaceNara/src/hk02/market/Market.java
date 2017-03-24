9ea6d614f90411eec1d088cd322cddd06ad3009f
hkedu <hkedu@192.168.2.11>
2017-03-24 16:36:03 +0900

    market menu시스템 insert 중

--- PeaceNara/src/hk02/market/Market.java
+++ PeaceNara/src/hk02/market/Market.java
@@ -8,10 +8,11 @@ import hk02.goods.Goods;
 import hk02.user.User;
 
 public class Market {
-	private ArrayList<Goods> goodsList = new ArrayList<Goods>(); // 총 상품리스트
+	private ArrayList<Goods> goodsList; // 총 상품리스트
 
 	public Market() { // 마켓 생성시 더미 항목들 입력.
 		int cnt = 1;
+		goodsList = new ArrayList<Goods>(); // 총 상품리스트
 		goodsList.add(new Goods("V20", "가개통", "LG-F800K", "LG", cnt++, 500000, "skd", "2017/03/23"));
 		goodsList.add(new Goods("갤럭시 S7", "6개월 사용", "SM-G930S", "삼성", cnt++, 450000, "ksw", "2017/03/23"));
 		goodsList.add(new Goods("G5", "1년 사용", "G5", "LG-F700S", cnt++, 300000, "skd", "2017/03/23"));
@@ -22,9 +23,7 @@ public class Market {
 	public void helloMarket() {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("*-PeaceNara에 오신걸 환영합니다.-*");
-		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("\t1.물품정보\t2.물품사기\t3.물품판매\t4.Main");
-		System.out.println("-----------------------------------------------------------------------");
+		Menu.helloMenuPrint();
 
 		while (true) {
 			System.out.println("\n>> 보고싶거나 하고싶은 번호를 입력해주세요.");
@@ -58,63 +57,124 @@ public class Market {
 		System.out.println("--물품 목록-----------------------------------------------------------------");
 		System.out.println(" 번호  제품명     가격   판매자     날짜         상태");
 		System.out.println("============================================================================");
-		for (int i = goodsList.size()-1; i >= 0; i--) {  // 역순으로 게시글을 보이게함.(최신글 순.)
+		for (int i = goodsList.size() - 1; i >= 0; i--) { // 역순으로 게시글을 보이게함.(최신글
+															// 순.)
 			System.out.println(goodsList.get(i));
 		}
 		System.out.println("----------------------------------------------------------------------------");
 	}
 
-	
-	public void buy(/* 유저가들어옴 */) {
+	public void buy() {
 		// user 객체로 buylist를 불러온다.
 		// 현재 임시로 user 객체를 생성
 		User user = new User("kyungss", "1234", 1000000);
+		Menu.buyMenuPrint();
+		
 		Scanner scan = new Scanner(System.in); // 지역변수로 써서 메소드 호출이후 메모리에서
 												// 삭제시키기위함.
+		int input = scan.nextInt();
+		switch (input) {
+		case 1:
+//			buy(유저정보)
+		case 2:
+			helloMarket();
+			break;
 
-			try {
-				System.out.println("- 구매하실 물품번호를 입력해주세요.");
-				int goodsNum = scan.nextInt();
+		default:
+			System.out.println("번호는 1~2까지만 있습니다..");
+			break;
+		}
+		
+		// 임시 코드
+		try {
+			System.out.println("- 구매하실 물품번호를 입력해주세요.");
+			int goodsNum = scan.nextInt();
 
-				if (goodsNum < goodsList.size() && goodsNum > 0
-						&& user.getBalance() >= goodsList.get(goodsNum - 1).getPrice()
-						&& goodsList.get(goodsNum - 1).getStock().equals("판매중")) {
+			if (goodsNum < goodsList.size() && goodsNum > 0
+					&& user.getBalance() >= goodsList.get(goodsNum - 1).getPrice()
+					&& goodsList.get(goodsNum - 1).getStock().equals("판매중")) {
 
-					user.setBalance(user.getBalance() - goodsList.get(goodsNum - 1).getPrice());
-					user.setBuyGoodsList(goodsList, (goodsNum - 1));
-					goodsList.get(goodsNum - 1).setStock(false);
+				user.setBalance(user.getBalance() - goodsList.get(goodsNum - 1).getPrice());
+				user.setBuyGoodsList(goodsList, (goodsNum - 1));
+				goodsList.get(goodsNum - 1).setStock(false);
 
-				} else if (goodsNum >= goodsList.size() || goodsNum <= 0) {
-					System.out.println("물품번호를 다시 확인하여 주십시오.\n");
+			} else if (goodsNum >= goodsList.size() || goodsNum <= 0) {
+				System.out.println("물품번호를 다시 확인하여 주십시오.\n");
 
-				} else if (goodsList.get(goodsNum - 1).getStock().equals("판매완료")) {
-					System.out.println("판매가 완료된 제품입니다.\n");
+			} else if (goodsList.get(goodsNum - 1).getStock().equals("판매완료")) {
+				System.out.println("판매가 완료된 제품입니다.\n");
 
-				} else if (user.getBalance() < goodsList.get(goodsNum - 1).getPrice()) {
-					System.out.println(goodsList.get(goodsNum - 1).getTitle() + "을 구매하시기에 소지하신 금액이 "
-							+ (goodsList.get(goodsNum - 1).getPrice() - user.getBalance()) + "만큼 부족합니다.\n");
-				}
-
-				System.out.println("소지하신 금액은 " + user.getBalance() + "\n");
-				goodsListPrint();
-
-			} catch (InputMismatchException e) {
-				scan.nextLine();
-				System.out.println("숫자만 입력해주세요.");
+			} else if (user.getBalance() < goodsList.get(goodsNum - 1).getPrice()) {
+				System.out.println(goodsList.get(goodsNum - 1).getTitle() + "을 구매하시기에 소지하신 금액이 "
+						+ (goodsList.get(goodsNum - 1).getPrice() - user.getBalance()) + "만큼 부족합니다.\n");
 			}
 
-			catch (Exception e) {
-				// TODO: handle exception
-				System.out.println(e);
-			}
+			System.out.println("소지하신 금액은 " + user.getBalance() + "\n");
+			goodsListPrint();
+
+		} catch (InputMismatchException e) {
+			scan.nextLine();
+			System.out.println("숫자만 입력해주세요.");
+		}
+
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e);
+		}
 
 		System.out.println("\n내가 구매한 목록 -----------------------------------------------------------");
 		for (Goods goods : user.getBuyGoodsList()) {
 			System.out.println(goods);
 		}
 		System.out.println("----------------------------------------------------------------------------");
+		
+		// 임시 코드까지
 
 	}
+	
+//	public void buy(/*유저정보보낸다.*/){
+//		try {
+//			System.out.println("- 구매하실 물품번호를 입력해주세요.");
+//			int goodsNum = scan.nextInt();
+//
+//			if (goodsNum < goodsList.size() && goodsNum > 0
+//					&& user.getBalance() >= goodsList.get(goodsNum - 1).getPrice()
+//					&& goodsList.get(goodsNum - 1).getStock().equals("판매중")) {
+//
+//				user.setBalance(user.getBalance() - goodsList.get(goodsNum - 1).getPrice());
+//				user.setBuyGoodsList(goodsList, (goodsNum - 1));
+//				goodsList.get(goodsNum - 1).setStock(false);
+//
+//			} else if (goodsNum >= goodsList.size() || goodsNum <= 0) {
+//				System.out.println("물품번호를 다시 확인하여 주십시오.\n");
+//
+//			} else if (goodsList.get(goodsNum - 1).getStock().equals("판매완료")) {
+//				System.out.println("판매가 완료된 제품입니다.\n");
+//
+//			} else if (user.getBalance() < goodsList.get(goodsNum - 1).getPrice()) {
+//				System.out.println(goodsList.get(goodsNum - 1).getTitle() + "을 구매하시기에 소지하신 금액이 "
+//						+ (goodsList.get(goodsNum - 1).getPrice() - user.getBalance()) + "만큼 부족합니다.\n");
+//			}
+//
+//			System.out.println("소지하신 금액은 " + user.getBalance() + "\n");
+//			goodsListPrint();
+//
+//		} catch (InputMismatchException e) {
+//			scan.nextLine();
+//			System.out.println("숫자만 입력해주세요.");
+//		}
+//
+//		catch (Exception e) {
+//			// TODO: handle exception
+//			System.out.println(e);
+//		}
+//		
+//		System.out.println("\n내가 구매한 목록 -----------------------------------------------------------");
+//		for (Goods goods : user.getBuyGoodsList()) {
+//			System.out.println(goods);
+//		}
+//		System.out.println("----------------------------------------------------------------------------");
+//	}
 
 	public void sell(/* 유저가들어옴 */) {
 		Scanner scan = new Scanner(System.in); // 지역변수로 써서 메소드 호출이후 메모리에서
@@ -139,9 +199,17 @@ public class Market {
 
 		goodsList.get(goodsList.size() - 1).setNumber(goodsList.size());
 
-		System.out.println("* 가격을 입력해주세요.");
-		input = scan.nextLine();
-		goodsList.get(goodsList.size() - 1).setPrice(Integer.parseInt(input));
+		while (true) {
+			try {
+				System.out.println("* 가격을 입력해주세요.");
+				input = scan.nextLine();
+				goodsList.get(goodsList.size() - 1).setPrice(Integer.parseInt(input));
+				break;
+			} catch (NumberFormatException e) {
+				// TODO: handle exception
+				System.out.println("\n!- 숫자로 입력해주세요.\n");
+			}
+		}
 
 		System.out.println("* 판매자 명을 입력해주세요.");
 		input = scan.nextLine();
---
+++ PeaceNara/src/hk02/market/Menu.java
@@ -0,0 +1,17 @@
+package hk02.market;
+
+public class Menu {
+	
+	public static void helloMenuPrint(){
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("\t1.물품정보\t2.물품사기\t3.물품판매\t4.Main");
+		System.out.println("------------------------------------------------------------------------");
+	}
+	
+	public static void buyMenuPrint(){
+		System.out.println("------------------------------------------------------------------------");
+		System.out.println("\t1.구매하실 물품 번호 입력\t2.Main");
+		System.out.println("------------------------------------------------------------------------");
+	}
+
+}
