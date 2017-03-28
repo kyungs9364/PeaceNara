525005de57c30fb262d9341d37a0d97437ae5ea5
송금동 <hankyung@192.168.2.28>
2017-03-28 17:55:35 +0900

    기종속성(model) 삭제 + 관련 메서드 삭제 viewGoods 클래스 출력부 수정 setCompany.java는 임시 클래스입니다.

--- PeaceNara/src/hk02/goods/ViewGoods.java
+++ PeaceNara/src/hk02/goods/ViewGoods.java
@@ -1,22 +1,21 @@
 package hk02.goods;
 
 public class ViewGoods {
-	public void viewGoods(Goods goods){		
+	public void viewGoods(Goods goods) {
 		System.out.println("----------------------------------------------------------------------------");
-		System.out.println("물품 번호 : "+goods.getNumber());
-		System.out.print(" 제    목 : "+goods.getTitle());
-		System.out.println("\t\t\t\t 게 시 일 : "+goods.getTime()+" "+goods.getTime2());
-		System.out.print(" 게 시 자 : "+goods.getSeller	());
-		System.out.println("\t\t\t\t\t 상    태 : "+goods.getStock());		
+		System.out.println(" 물품번호 : " + goods.getNumber());
+		System.out.println(" 제    목 : " + goods.getTitle());
+		System.out.println("\t\t\t\t\t\t 게 시 일 : " + goods.getTime() + " " + goods.getTime2());
+		System.out.print(" 게 시 자 : " + goods.getSeller());
+		System.out.println("\t\t\t\t\t 상    태 : " + goods.getStock());
 		System.out.println("----------------------------------------------------------------------------");
-		System.out.print(" 내    용 : ");
-		for(int i=0;i<goods.content.size();i++){
-			System.out.println(goods.content.get(i));
+		for (int i = 0; i < goods.content.size(); i++) {
+			System.out.println(" "+goods.content.get(i));
 		}
+		System.out.println();
 		System.out.println("----------------------------------------------------------------------------");
-		System.out.print(" 가    격 : "+goods.getPrice());
-		System.out.print("\t\t\t 기    종 : "+goods.getModel());
-		System.out.println("\t 제 조 사 : "+goods.getCompany());
+		System.out.print(" 가    격 : " + goods.getPrice()+" 원");		
+		System.out.println("\t\t\t\t 제 조 사 : " + goods.getCompany());
 		System.out.println("----------------------------------------------------------------------------");
 		System.out.println();
 	}
