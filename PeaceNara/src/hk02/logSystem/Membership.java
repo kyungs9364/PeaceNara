ee1cad8ba84f90c599815af391d8e401ed3e1127
hankyung <hankyung@192.168.2.23>
2017-03-28 18:13:06 +0900

    회원탈퇴중-정해령

--- PeaceNara/src/hk02/logSystem/Membership.java
+++ PeaceNara/src/hk02/logSystem/Membership.java
@@ -13,36 +13,37 @@ public class Membership {
 
 	User user = new User();
 	List<User> userList;
+	Login login;
 
 	public Membership() {
 		this.userList = new ArrayList<>();
-		// id = user.getId();
-		// pw=user.getPw();
-		// pwCheck=user.getPwCheck();
+		userList.add(new User("jhrrr", "a123123", "a123123", "010-1234-5678", 500000));
+		userList.add(new User("yksss", "b123123", "b123123", "010-1234-5678", 300000));
 	}
 
 	public void memberMenu() {
-		System.out.println("-------------------");
-		System.out.println("1.로그인    2.회원가입");
-		System.out.println("-------------------");
+
 		while (true) {
+			System.out.println("-------------------");
+			System.out.println("1.로그인    2.회원가입");
+			System.out.println("-------------------");
 			try {
 				num = sc.nextInt();
-				Login login = new Login(userList);
-
+				login = new Login(userList);
 				switch (num) {
 				case 1:
 					login.login();
 					break;
 				case 2:
 					input();
-					list();
+					list(userList);
 					login.login();
 					break;
-				}
-				if (num != 1 || num != 2) {
+
+				default:
 					System.out.println("1과 2 중에 입력하세요");
 				}
+
 			} catch (InputMismatchException e) {
 				System.out.println("숫자를 입력하세요");
 				sc.nextLine();
@@ -50,91 +51,95 @@ public class Membership {
 		}
 	}
 
-	public void id() {
-
-		// while (true) {
-		System.out.print("아이디 : ");
-		user.setId(sc.next());
-		if (user.getId().length() > 10) {
-			System.out.println("열글자 이하로 입력하세요.");
-		}
-		if (user.getId().length() < 5) {
-			System.out.println("다섯글자 이상으로 입력하세요.");
-		}
-		sc.nextLine();
-		// }
-	}
-
-	public void pw() {
-		// while (true) {
-		System.out.print("비밀번호 : ");
-		user.setPw(sc.nextLine());
-		if (user.getPw().length() > 10) {
-			System.out.println("열다섯글자 이하로 입력하세요.");
-		}
-		if (user.getPw().length() < 5) {
-			System.out.println("다섯글자 이상으로 입력하세요.");
-		}
-		sc.nextLine();
-		// }
-	}
-
-	public void pwCheck() {
-		// while (true) {
-		System.out.println("비밀번호 확인 : ");
-		user.setPwCheck(sc.nextLine());
-		if (!user.getPw().equals(user.getPwCheck())) {
-			System.out.println("다시 확인해 주세요");
-		}
-		sc.nextLine();
-		// }
-	}
-
-	public void cellPhone() {
-		System.out.println("핸드폰번호 : ");
-		user.setCellPhone(sc.nextLine());
-	}
-
-	public void balance() {
-		System.out.println("잔액 : ");
-
-		try {
-			user.setBalance(sc.nextInt());
-		} catch (InputMismatchException e) {
-			System.out.println("숫자를 입력하세요");
-			sc.nextLine();
-		}
-	}
-
-	public void member() {
-		userList.add(new User("jhr", "a123", "a123", "010-1234-1234", 500000));
-		userList.add(new User("yks", "b123", "b123", "010-1234-5678", 300000));
+	public boolean isCellphone(String str) {
+		return str.matches("(01[016789])-(\\d{3,4})-(\\d{4})");
 	}
 
 	public void input() {
 		System.out.println("---------회원가입---------");
-		id();
-		pw();
-		pwCheck();
-		cellPhone();
-		balance();
+		while (true) {
+			System.out.print("아이디 : ");
+			String id = sc.nextLine().replaceAll("\\s", "");
+			if (id.length() > 10) {
+				System.out.println("열글자 이하로 입력하세요.");
+				continue;
+			}
+			if (id.length() < 5) {
+				System.out.println("다섯글자 이상으로 입력하세요.");
+				continue;
+			}
 
-		System.out.println("등록하시겠습니까?(y/n)");
-		if (sc.nextLine().equals("y")) {
-			userList.add(
-					new User(user.getId(), user.getPw(), user.getPwCheck(), user.getCellPhone(), user.getBalance()));
-			System.out.println("감사합니다. 가입되셨습니다.");
-		} else {
-			System.out.println("등록이 취소되었습니다.");
+			user.setId(id);
+			break;
+		}
+
+		String pw = "";
+		while (true) {
+			System.out.print("비밀번호 : ");
+			pw = sc.nextLine().replaceAll("\\s", "");
+			if (pw.length() > 10) {
+				System.out.println("열다섯글자 이하로 입력하세요.");
+				continue;
+			}
+			if (pw.length() < 4) {
+				System.out.println("네글자 이상으로 입력하세요.");
+				continue;
+			}
+			user.setPw(pw);
+			break;
+		}
+
+		while (true) {
+			System.out.println("비밀번호 확인 : ");
+			String pwCheck = sc.nextLine().replaceAll("\\s", "");
+			if (!pw.equals(pwCheck)) {
+				System.out.println("다시 확인해 주세요");
+				continue;
+			}
+			user.setPwCheck(pwCheck);
+			break;
+		}
+
+		while (true) {
+			System.out.println("핸드폰번호 : ");
+			String cellPhone = sc.nextLine().replaceAll("\\s", "");
+			isCellphone(cellPhone);
+			user.setCellPhone(cellPhone);
+			break;
+		}
+
+		while (true) {
+			System.out.println("잔액 : ");
+			int balance = sc.nextInt();
+			try {
+				user.setBalance(balance);
+			} catch (InputMismatchException e) {
+				System.out.println("숫자를 입력하세요");
+				sc.nextLine();
+			}
+			break;
+		}
+
+		while (true) {
+			System.out.println("등록하시겠습니까?(y/n)");
+			String str = sc.next();
+			if (str.equalsIgnoreCase("y")) {
+				userList.add(new User(user.getId(), user.getPw(), user.getPwCheck(), user.getCellPhone(),
+						user.getBalance()));
+				System.out.println("감사합니다. 가입되셨습니다.");
+			} else if (str.equalsIgnoreCase("n")) {
+				System.out.println("등록이 취소되었습니다.");
+			} else {
+				System.out.println("다시 입력해주세요.");
+				continue;
+			}
+			break;
 		}
 	}
 
-	public void list() {
-		member();
+	public void list(List<User> userList) {
 		for (int i = 0; i < userList.size(); i++) {
 			System.out.println(userList.get(i).toString());
 		}
-
 	}
-
 }
